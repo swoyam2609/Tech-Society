480785440b451db068b9ab74fef0e138003fb837
amanmodiii <amanmodi290903@gmail.com>
2023-09-09 19:23:48 +0530

    header and footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import logo from "../../assets/logo.svg"
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer-container w-screen">
+      <div className="min-h- mx-auto sm:w-1/2 sm:flex p-5 justify-evenly text-white">
+        <div className=''>
+          <div className="items-center font-[Poppins] text-white">
+            <a href="/" className="flex">
+              <span>
+                <img src={logo} alt="tech-society logo" />
+              </span>
+              <span className="pl-2 font-sans">
+                <h2 className="text-3xl mb-1 tracking-wide">
+                  <span className="font-bold">Tech</span>Society
+                </h2>
+                <h5 className="text-xs text-sm text-center  tracking-widest">
+                  IIIT BHUBANESWAR
+                </h5>
+              </span>
+            </a>
+            <p className=" text-[#848484] mr-5 tracking-wide leading-6 text-xs line py-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className='px-10 lg:px-6 py-2'>
+          <h2 className="text-md font-bold">LINKS</h2>
+          <ul className="flex space-x-8">
+            <li className="text-[#848484] text-xs">
+              <ul className="space-y-3 py-3 min-w-max">
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/about">About</a>
+                </li>
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/team">Our Team</a>
+                </li>
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/gallery">Gallery</a>
+                </li>
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+            </li>
+            <li className="text-[#848484] text-xs">
+              <ul className="space-y-3 py-3 min-w-max">
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/">Help & Support</a>
+                </li>
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/">Privacy Policy</a>
+                </li>
+                <li className='transition-all duration-300 hover:text-white'>
+                  <a href="/gal">Terms of Use</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-col px-5 pb-2 md:flex-row justify-evenly items-center w-full bg-black">
+        <div className="text-[#848484] flex justify-center text-sm p-2">
+          © Copyright, 2023. Tech Society-IIIT Bhubaneswar
+        </div>
+        <div className="text-white justify-center text-lg flex space-x-2">
+          <ion-icon name="logo-twitter"></ion-icon>
+          <ion-icon name="logo-facebook"></ion-icon>
+          <ion-icon name="logo-instagram"></ion-icon>
+          <ion-icon name="mail"></ion-icon>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,23 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../../assets/logo.svg"
 
 export default function NavBar() {
+    let [isOpen, setIsOpen ] = useState(false);
+    let [dropdown, setDropdown] = useState(false);
+
   return (
-    <div className="w-full top-0 left-0 navbar">
-      <div className="md:flex bg-[#0c0a0a] py-4 px-7 md:px-10">
-        <div className="cursor-pointer flex items-center font-[Poppins] text-white">
-          <span>
-            <img src={logo} alt="tech-society logo" />
-          </span>
-          <span className="pl-2 font-sans">
-            <h2 className="text-3xl mb-1 tracking-wide">
-              <span className="font-bold">Tech</span>Society
-            </h2>
-            <h5 className="text-xs text-center  tracking-widest">
-              IIIT BHUBANESWAR
-            </h5>
-          </span>
+    <div className=" shadow-md w-full top-0 left-0 navbar">
+      <div className="md:flex items-center justify-between bg-[#0c0a0a] py-4 px-7 md:px-10">
+        <div className="items-center font-[Poppins] text-white">
+          <a href="/" className="flex">
+            <span>
+              <img src={logo} alt="tech-society logo" />
+            </span>
+            <span className="pl-2 font-sans">
+              <h2 className="text-3xl mb-1 tracking-wide">
+                <span className="font-bold">Tech</span>Society
+              </h2>
+              <h5 className="text-xs text-center  tracking-widest">
+                IIIT BHUBANESWAR
+              </h5>
+            </span>
+          </a>
         </div>
+        <div
+          className="text-3xl text-white absolute right-8 top-6 cursor-pointer md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <ion-icon name={isOpen ? "close" : "menu"}></ion-icon>
+        </div>
+        <ul
+          className={`text-[#848484] bg-[#0c0a0a] md:flex md:items-center md:py-0 py-4 md:static absolute left-0 w-full md:w-auto md:pl-0 pl-5 md:z-auto z-[-1] transition-all duration-500 ease-in ${
+            isOpen ? "top-20 z-auto" : "top-[-500px]"
+          }`}
+        >
+          <li className="md:ml-8 text-sm md:my-0 my-5">
+            <a href="/" className="hover:text-white duration-300">
+              HOME
+            </a>
+          </li>
+          <li className=" relative md:top-1.5 md:ml-8 text-sm md:my-0 my-5">
+            <button
+              className="hover:text-white duration-300 pb-3"
+              onMouseEnter={() => setDropdown(true)}
+            >
+              PAGES &#709;
+            </button>
+            <div
+              className={`w-28 md:absolute bg-[#0c0c0a] -right-10 p-2 transition-all duration-1000 ${
+                dropdown ? "top-15" : "top-[-200px]"
+              }`}
+              onMouseLeave={() => setDropdown(false)}
+            >
+              <ul className="space-y-2">
+                <li>
+                  <a
+                    href="/about"
+                    className="flex duration-300 hover:text-white"
+                  >
+                    ABOUT US
+                  </a>
+                </li>
+                <li>
+                  <a href="/faq" className="flex duration-300 hover:text-white">
+                    FAQs
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className="md:ml-8 text-sm md:my-0 my-5">
+            <a href="/gallery" className="hover:text-white duration-300">
+              GALLERY
+            </a>
+          </li>
+          <li className="md:ml-8 text-sm md:my-0 my-5">
+            <a href="/events" className="hover:text-white duration-300">
+              EVENTS
+            </a>
+          </li>
+          <li className="md:ml-8 text-sm md:my-0 my-5">
+            <a href="/contact" className="hover:text-white duration-300">
+              CONTACT
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
