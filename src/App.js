07fbbf2a4ae8f43a4cc03c42ea68e7067c624b2f
amanmodiii <amanmodi290903@gmail.com>
2023-09-09 12:01:16 +0530

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
-import NavBar from './components/navbar/NavBar';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Events from "./pages/events/Events";
+import Faq from "./pages/faq/Faq"
+import Gallery from "./pages/gallery/Gallery"
+import Team from "./pages/team/Team"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      App
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import logo from "../../assets/logo.svg"
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div className="w-full top-0 left-0 navbar">
+      <div className="md:flex bg-[#0c0a0a] py-4 px-7 md:px-10">
+        <div className="cursor-pointer flex items-center font-[Poppins] text-white">
+          <span>
+            <img src={logo} alt="tech-society logo" />
+          </span>
+          <span className="pl-2 font-sans">
+            <h2 className="text-3xl mb-1 tracking-wide">
+              <span className="font-bold">Tech</span>Society
+            </h2>
+            <h5 className="text-xs text-center  tracking-widest">
+              IIIT BHUBANESWAR
+            </h5>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
