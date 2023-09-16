8af1bf5a1a3a692df1cd480455f4350769ff7eb7
Jaya-Shinra Tensei <102919369+JayadipSahoo@users.noreply.github.com>
2023-09-16 22:14:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "standard-bg": "url('./assets/bg.jpeg')"
+         "standard-bg-2": "url('./assets/standard-bg-2.png')"
       },
       fontSize: {
         '4.5xl': '2.5rem', // You can adjust the size as needed
